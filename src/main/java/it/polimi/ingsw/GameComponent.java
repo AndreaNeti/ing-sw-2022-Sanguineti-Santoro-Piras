34c35ca505f648b51ac3e3c060114d89cf7a1f38
Andrea Sanguineti <logisimit@gmail.com>
2022-03-15 14:09:00 +0100

    UML updated, GameComponent, Piece

---
+++ src/main/java/it/polimi/ingsw/GameComponent.java
@@ -0,0 +1,6 @@
+package it.polimi.ingsw;
+
+public interface GameComponent {
+    public void addPiece(Piece p);
+    public void removePiece(Piece p);
+}
---
+++ src/main/java/it/polimi/ingsw/Piece.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw;
+
+public class Piece {
+    private Color color;
+    private GameComponent component;
+
+    public Color getColor(){
+        return this.color;
+    }
+
+    public void movePiece(GameComponent gc){
+    }
+}
