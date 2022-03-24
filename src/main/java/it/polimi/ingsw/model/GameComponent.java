9c45d87878087c3d0cf55d7698c2a1bb61d8d692
Emanu <emanuele1.santoro@mail.polimi.it>
2022-03-24 12:39:33 +0100

    merge island

--- src/main/java/it/polimi/ingsw/model/GameComponent.java
+++ src/main/java/it/polimi/ingsw/model/GameComponent.java
@@ -10,25 +10,25 @@ public abstract class GameComponent {
         this.students = new byte[]{0, 0, 0, 0, 0};
     }
 
-    public void addStudents(byte color, byte number) {
-        students[color] += number;
+    public void addStudents(Color color, byte number) {
+        students[color.ordinal()] += number;
     }
 
-    private void removeStudents(byte color, byte number) throws NotEnoughStudentsException {
-        if(students[color] > number)
-            students[color] -= number;
+    public void removeStudents(Color color, byte number) throws NotEnoughStudentsException {
+        if(students[color.ordinal()] > number)
+            students[color.ordinal()] -= number;
         else
             throw new NotEnoughStudentsException();
     }
 
     public void moveStudents(Color color, byte number, GameComponent component) throws NotEnoughStudentsException{
-        byte index = (byte) color.ordinal();
-        this.removeStudents(index, number);
-        component.addStudents(index, number);
+        this.removeStudents(color, number);
+        component.addStudents(color, number);
     }
 
 
     public byte[] getStudents() {
+
         return students.clone();
     }
 
--- src/main/java/it/polimi/ingsw/model/Island.java
+++ src/main/java/it/polimi/ingsw/model/Island.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.exceptions.NotEnoughStudentsException;
+
 public class Island extends GameComponent{
     private Player player;
     private boolean prohibition;
@@ -7,12 +9,37 @@ public class Island extends GameComponent{
 
     public Island() {
         super();
+        player=null;
+        prohibition=false;
+        number=1;
+
+
+    }
+
+    public void setProhibition(boolean prohibition) {
+        this.prohibition = prohibition;
     }
 
     public Player getPlayer() {
         return player;
     }
-    public void merge(Island island){}
+
+    //merge serve per mandare tutti i propri studenti su un'altra isola: il try catch è dovuto al fatto che
+    //l'isola sa precisamente quanti studenti ha e quindi non potrà mai lanciarla come eccezion
+    public void merge(Island island){
+        byte[] students= getStudents();
+
+        for (Color c: Color.values()) {
+            try{
+                moveStudents(c, students[c.ordinal()], island);
+            }catch (NotEnoughStudentsException ex){
+                System.out.println("qualcosa");
+            }
+
+        }
+
+
+    }
 
     public byte getNumber() {
         return number;
