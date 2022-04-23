f41a559cf988d2d9db2f15eb683c01ccc3d753dc
Andrea Sanguineti <andrea.sanguineti@mail.polimi.it>
2022-04-23 18:18:22 +0200

    Added join, create, remove matches methods in Server

--- src/main/java/it/polimi/ingsw/controller/Server.java
+++ src/main/java/it/polimi/ingsw/controller/Server.java
@@ -1,12 +1,15 @@
 package it.polimi.ingsw.controller;
 
+import it.polimi.ingsw.exceptions.NotAllowedException;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.util.HashMap;
-import java.util.List;
+import java.util.LinkedHashMap;
 
 public class Server {
-    public static HashMap<MatchType, HashMap<Integer, List<Controller>>> matches;
+    public static HashMap<MatchType, LinkedHashMap<Long, Controller>> matches;
+    public static Long matchId = 0L;
 
     public static void main(String[] args) {
         matches = new HashMap<>();
@@ -25,4 +28,33 @@ public class Server {
         }
     }
 
+    public static Controller createMatch(MatchType matchType) {
+        LinkedHashMap<Long, Controller> filteredMatches = matches.computeIfAbsent(matchType, k -> new LinkedHashMap<>());
+        Controller c = new Controller(matchType);
+        filteredMatches.put(matchId, c);
+        matchId++;
+        return c;
+    }
+
+    public static Controller joinMatch(MatchType matchType) throws NotAllowedException {
+        LinkedHashMap<Long, Controller> filteredMatches = matches.get(matchType);
+        if (filteredMatches == null) throw new NotAllowedException("No matches found :(");
+        // get oldest
+        Controller c = (Controller) filteredMatches.entrySet().iterator().next();
+        if (c == null) throw new NotAllowedException("No matches found :(");
+        return c;
+    }
+
+    public static Controller joinMatchById(Long id) throws NotAllowedException {
+        Controller c = null;
+        for (LinkedHashMap<Long, Controller> matches : matches.values())
+            if ((c = matches.get(id)) != null) break;
+        if (c == null) throw new NotAllowedException("No matches found :(");
+        return c;
+    }
+
+    public static void removeMatch(Long id) {
+        for (LinkedHashMap<Long, Controller> matches : matches.values())
+            if (matches.remove(id) != null) break;
+    }
 }
