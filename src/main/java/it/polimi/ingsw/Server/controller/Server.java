94f176f8aac65139f08e704027a58e77a9669d46
Emanu <emanuele1.santoro@mail.polimi.it>
2022-05-09 20:47:17 +0200

    small fix

--- src/main/java/it/polimi/ingsw/Server/controller/Server.java
+++ src/main/java/it/polimi/ingsw/Server/controller/Server.java
@@ -5,6 +5,7 @@ import it.polimi.ingsw.exceptions.NotAllowedException;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static java.util.Map.entry;
 
@@ -13,7 +14,7 @@ public class Server {
     public static Long matchId = 0L;
     public static final ConcurrentHashMap<MatchType, Map<Long, Controller>> matches = new ConcurrentHashMap<>();
     private static final Set<String> nickNames = Collections.synchronizedSet(new HashSet<>());
-    private static final ConcurrentHashMap<MatchType, MatchConstants> matchConstants = (ConcurrentHashMap<MatchType, MatchConstants>) Map.ofEntries(
+    private static final Map<MatchType, MatchConstants> matchConstants =  Map.ofEntries(
             entry(new MatchType((byte) 2, false), new MatchConstants(10, 3, 0, 0, 0, 7, 10, 8, 3)),
             entry(new MatchType((byte) 2, true), new MatchConstants(10, 3, 3, 20, 1, 7, 10, 8, 3)),
             entry(new MatchType((byte) 3, false), new MatchConstants(10, 3, 0, 0, 0, 9, 10, 6, 4)),
