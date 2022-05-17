24fedfd8c84d223a28dac4c9dd13d9fd93e284c1
Andrea Piras <andrea.piras00@gmail.com>
2022-05-17 14:52:18 +0200

    Added structure of all commands

---
+++ src/main/java/it/polimi/ingsw/Client/PhaseAndComand/Commands/SetCharacterInputCommand.java
@@ -0,0 +1,26 @@
+package it.polimi.ingsw.Client.PhaseAndComand.Commands;
+
+import it.polimi.ingsw.Client.View.AbstractView;
+
+import java.awt.event.ActionEvent;
+
+public class SetCharacterInputCommand extends GameCommand{
+    public SetCharacterInputCommand(AbstractView view) {
+        super(view);
+    }
+
+    @Override
+    void playCLICommand() {
+
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
